eeef9936633e75dc1be7ca740c31beecb43329dc
Ankit Jaiswal <ankitjaiswal@Ankits-MacBook-Air.local>
2021-05-28 19:06:42 +0545

    corrected error mongodb_uri

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     production : {
         SECRET : process.env.SECRET,
-        DATABASE : process.env.DATABASE_URI
+        DATABASE : process.env.MONGODB_URI
     },
     default: {
         SECRET : 'SUPERSECRET',
